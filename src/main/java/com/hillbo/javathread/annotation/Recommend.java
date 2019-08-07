3f7cc0c15b92a11bb101622b4132aa6450ed3d7c
zhaohaibo <hillbo.zhao@foxmail.com>
2019-08-08 00:32:32 +0800

    add annotations

---
+++ src/main/java/com/hillbo/javathread/annotation/Recommend.java
@@ -0,0 +1,23 @@
+package com.hillbo.javathread.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 用来标记【推荐】的类或者是写法
+ */
+
+// Target指注解作用的目标
+@Target(ElementType.TYPE)
+
+// Retention指注解存在的范围
+// SOURCE在编译的时候会被忽略掉
+// RUNTIME标识注解会在字节码文件中存在，在运行时通过反射可以拿到
+@Retention(RetentionPolicy.SOURCE)
+public @interface Recommend {
+
+    String value() default "";
+
+}
